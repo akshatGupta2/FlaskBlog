6df6f6d11478b1c2361b11eda56b3dc6c21a663c
Akshat Gupta <akshat2032003@gmail.com>
2025-09-06 15:24:19 +0530

    Hiding all the secrets

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 from os import getenv
+
 class Config:
-    SECRET_KEY = 'caa4666641a41891d231c5dc39174e24'
+    SECRET_KEY = getenv("SECRET")
     SQLALCHEMY_DATABASE_URI = getenv("SQL_URI")
 
     # or "sqlite:///C:\\Users\\aksha\\Desktop\\Flask_Tutorial\\flashy_blogs.db"
