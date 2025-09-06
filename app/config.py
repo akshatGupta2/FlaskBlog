9c16b248391b958c1a79029dcf8229ba77916bc0
Akshat Gupta <akshat2032003@gmail.com>
2025-09-06 13:02:25 +0530

    Making it work!

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 from os import getenv
 class Config:
     SECRET_KEY = 'caa4666641a41891d231c5dc39174e24'
-    SQLALCHEMY_DATABASE_URI = getenv("SQL_URI")
+    SQLALCHEMY_DATABASE_URI = "postgresql://neondb_owner:npg_SoRf3ms4duVh@ep-solitary-river-a19htapa-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
     # or "sqlite:///C:\\Users\\aksha\\Desktop\\Flask_Tutorial\\flashy_blogs.db"
